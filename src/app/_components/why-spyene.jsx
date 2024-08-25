8d07e166d73d75c4ce17766669327766af618532
sadat66 <imteaz.sadat@corexlab.dev>
2024-08-25 17:00:19 +0600

    update

--- src/app/_components/why-spyene.jsx
+++ src/app/_components/why-spyene.jsx
@@ -1,7 +1,36 @@
-const WhySpyene = () => {
-    return ( <div>
-        Why Spyene! 
-    </div> );
+
+
+import Image from 'next/image';
+
+const images = [
+  '/image1.png',
+  '/image2.png',
+  '/image3.png',
+  '/image4.png',
+  '/image5.png',
+  '/image6.png',
+];
+
+ function WhySpyene() {
+  return (
+    <div className='flex w-[1440px] flex-col items-start gap-[128px] bg-white'>
+    
+
+    <div className="px-[256px] py-[128px] grid grid-cols-3 gap-4">
+    
+      {images.map((src, index) => (
+        <div key={index} className="w-full h-auto">
+          <Image 
+            src={src} 
+            alt={`Image ${index + 1}`} 
+            width={304} 
+            height={215} 
+            className="object-cover w-full h-full" 
+          />
+        </div>
+      ))}
+    </div>
+    </div>
+  );
 }
- 
 export default WhySpyene;
