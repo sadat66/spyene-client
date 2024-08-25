f6c0221709b4f13ee1952481d5357a9c04f09340
sadat66 <imteaz.sadat@corexlab.dev>
2024-08-25 16:22:20 +0600

    update

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -9,12 +9,12 @@ const Navbar = () => {
         <div className="flex gap-10 justify-center items-center pr-56 pl-56 w-full text-sm font-medium leading-none whitespace-nowrap bg-white min-h-[60px] text-slate-900 max-md:px-5 max-md:max-w-full">
           <div className="flex flex-col self-stretch px-8 my-auto min-w-[240px] w-[989px] max-md:px-5 max-md:max-w-full">
             <div className="flex flex-wrap gap-10 justify-between items-center py-4 w-full border-b border-white min-h-[60px] max-md:max-w-full">
-            <Image 
-        src="/SPYENE.png" // Path to your image file in the public directory
-        alt="SPYENE" // A description of the image for accessibility
-        width={82} // Set the desired width of the image
-        height={28} // Set the desired height of the image
-      />
+                    <Image 
+                src="/SPYENE.png" // Path to your image file in the public directory
+                alt="SPYENE" // A description of the image for accessibility
+                width={82} // Set the desired width of the image
+                height={28} // Set the desired height of the image
+              />
             
               <div className="flex gap-6 justify-between items-center self-stretch my-auto min-w-[240px] w-[308px]">
                 <div className="flex gap-4 items-center self-stretch h-full">
