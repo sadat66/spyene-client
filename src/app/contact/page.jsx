9847e31f2873cb76b629f533fb15bf1b81a296c1
sadat66 <imteaz.sadat@corexlab.dev>
2024-08-26 16:57:55 +0600

    updated contacts

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,11 +1,11 @@
-import Body from "./body";
+
 import FAQ from "../_components/faq";
 import GetQuotation from "../_components/get-quotation";
 
 const ContactPage = () => {
     return ( <div className="flex max-w-[1440px] w-full flex-col items-center mx-auto">
        
-        <Body/>
+        
         <GetQuotation/>
         <FAQ/>
     </div> );
