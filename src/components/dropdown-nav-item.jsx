f5d95fa318d9da6c77a08d19fd2a0fb1c2409bf7
zubaerSumon <zubaer@corexlab.dev>
2024-08-26 17:53:48 +0600

    drop-down menu added

---
+++ src/components/dropdown-nav-item.jsx
@@ -0,0 +1,47 @@
+"use client";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ArrowRight } from "lucide-react";
+import { Fragment, useState } from "react";
+import Link from "next/link";
+
+export function DropdownNavItem(props) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <DropdownMenu open={open}>
+      <div /* onMouseLeave={handleMouseLeave} */>
+        <DropdownMenuTrigger
+          onMouseEnter={() => setOpen(true)}
+          className="outline-none"
+        >
+          {props.title}
+        </DropdownMenuTrigger>
+        <DropdownMenuContent
+          onMouseLeave={() => setOpen(false)}
+          className={`w-56 mt-3 `}
+        >
+          {props?.menus?.map((menu) => (
+            <Fragment key={menu?.id}>
+              <DropdownMenuItem>
+                <Link href="#" className="hover:text-red-600">
+                  {menu.attributes.Title}
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+            </Fragment>
+          ))}
+          <DropdownMenuItem>
+            <span>View All</span>
+            <ArrowRight className="ms-2 h-3 w-3" />
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </div>
+    </DropdownMenu>
+  );
+}
