471bc8aa070e233516f8670cc854504f55c3ea65
sadat66 <imteaz.sadat@corexlab.dev>
2024-08-27 13:24:32 +0600

    added Residential Page

---
+++ src/app/residential/page.jsx
@@ -0,0 +1,168 @@
+const Residential = () => {
+    return ( <>
+
+
+<div className="flex w-full max-w-[928px] mx-auto flex-col text-sm font-normal leading-5 text-slate-900 max-md:px-5 pt-[128px]">
+  <h2 className="text-2xl font-bold mb-4">Safe Installation for A Safe Home</h2>
+  <p>
+  Safe installation is crucial for a secure home. At Spyene Technologies, we prioritize safety in every project. Our expert electricians ensure that all work is done with precision and no errors. We handle everything from smoke alarm setup to safety switch installation. We also specialize in switchboard upgrades and socket installation. Every home deserves the best protection, and we deliver it. With our focus on safety, residents can have peace of mind knowing their electrical systems are in expert hands.
+  </p>
+</div>
+
+
+
+
+
+
+
+<div className="flex w-full max-w-[928px] mx-auto flex-col text-sm font-normal leading-5 text-slate-900 max-md:px-5 pt-[128px]">
+  <h2 className="text-2xl font-bold mb-4">Energy-Efficient Lighting Solutions</h2>
+  <p>
+  Upgrade your home with energy-efficient lighting solutions. We offer indoor and outdoor light installation services that make the home brighter and safer. Enjoy general lighting for everyday use, and increase room aesthetics with stylish ceiling lights and track lighting. For focus lights, we have got you covered with task lighting for reading or working. With our solutions, there’s no more fear of darkness. Host parties outdoors with friends and family with confidence. We ensure energy saving without compromising on brightness or quality.
+  </p>
+</div>
+
+
+
+
+
+<div className="flex w-full max-w-[928px] mx-auto flex-col text-sm font-normal leading-5 text-slate-900 max-md:px-5 pt-[128px]">
+  <h2 className="text-2xl font-bold mb-4">Safe and Reliable Electrical Repairs</h2>
+  <p>
+  Our experts are dedicated to solving all kinds of electrical issues. We’re available anytime. With quick arrival times, we ensure your home is safe and functional again fast. Our team provides quick solutions to minimize disruption. We also offer budget-friendly solutions without compromising quality. Trust us to keep residential electrical systems running smoothly.
+  </p>
+</div>
+
+
+
+<div className="flex w-full max-w-[928px]  mx-auto flex-col text-sm font-normal leading-5 text-slate-900 max-md:px-5">
+
+<div className="pt-[128px] justify-center items-center pb-[128px] max-w-[928px] ">
+  <h2 className="text-2xl font-bold mb-4">Our Services</h2>
+  
+  <div className="flex justify-between">
+    <div className="w-1/2 ">
+      <ul className="list-disc pl-5">
+        <li>Air Condition Installation & Repair</li>
+        <li>CCTV and home security installation</li>
+        <li>Ceiling Fans Circuit breaker upgrades</li>
+        <li>EV charging station installation</li>
+        <li>Electrical panel upgrades</li>
+        <li>Electrical safety inspections</li>
+        <li>Electrical system inspections</li>
+        <li>Energy-efficient lighting solutions</li>
+        <li>Fuse Box Repairs </li>
+        <li>Home automation system installation</li>
+        <li>Home theater wiring and installation</li>
+        <li>House wiring and rewiring</li>
+        
+        
+      </ul>
+    </div>
+    
+    <div className="w-1/2">
+      <ul className="list-disc pl-5">
+        <li>Hot tub and spa wiring</li>
+        <li>Hot tub and spa wiring</li>
+        <li>Outlet and switch installation</li>
+        <li>Generator installation and maintenance</li>
+        <li>Safety Switches</li>
+        <li>Smart home device integration</li>
+        <li>Smoke Alarm Installation</li>
+        <li>Socket installation and repair</li>
+        <li>Switchboard Installation</li>
+        <li>Underground Power</li>
+        <li>Whole Home Surge Protection</li>
+        
+        
+      </ul>
+    </div>
+  </div>
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+<div className="flex w-full max-w-[928px] mx-auto flex-col text-sm font-normal leading-5 text-slate-900 max-md:px-5">
+  <h2 className="text-2xl font-bold mb-4">Affordable Residential Electrical Maintenance</h2>
+  <p>
+  We provide the best prices for all residential electrical needs at budget-friendly prices without sacrificing quality. Clients can get a free quote from us to compare with other companies. We are confident in our value, so we encourage our clients to compare our quotes. The quotes include a detailed breakdown with no hidden costs to ensure transparency. With us, residents can keep their home’s electrical systems in the optimal condition without breaking the bank.
+  </p>
+</div>
+
+
+
+
+
+
+
+
+
+
+<div className="pt-[128px] flex w-full max-w-[928px] mx-auto flex-col text-sm font-normal leading-5 text-slate-900 max-md:px-5">
+  <h2 className="text-2xl font-bold mb-4">Why Choose Spyene Technologies for Home Electrical Needs?</h2>
+  <p>
+    We are local to NSW and understand the needs of the state. Our team includes expert, trained, skilled staff who are certified and verified. We pride ourselves on delivering top-notch service to ensure customer satisfaction. Our happy customers are the evidence of our dedication and commitment. Let us handle any commercial electrical needs with professionalism and care.
+  </p>
+</div>
+
+
+
+
+
+
+    <div className="flex w-full max-w-[928px]  mx-auto flex-col text-sm font-normal leading-5 text-slate-900 max-md:px-5">
+
+<div className="pt-[128px] justify-center items-center pb-[128px] max-w-[928px] ">
+  <h2 className="text-2xl font-bold mb-4">We Are Available Across NSW</h2>
+  <p className="mb-6">
+    We are available across NSW to meet all your electrical needs. Our services extend throughout the state, including:
+  </p>
+  
+  <div className="flex justify-between">
+    <div className="w-1/2 ">
+      <ul className="list-disc pl-5">
+        <li>Sydney</li>
+        <li>Newcastle</li>
+        <li>Wollongong</li>
+        <li>Central Coast</li>
+        <li>Coffs Harbour</li>
+        <li>Tamworth</li>
+        <li>Albury</li>
+        <li>Orange</li>
+        
+      </ul>
+    </div>
+    
+    <div className="w-1/2">
+      <ul className="list-disc pl-5">
+        <li>Bathurst</li>
+        <li>Dubbo</li>
+        <li>Wagga Wagga</li>
+        <li>Port Macquire</li>
+        <li>Nowra</li>
+        <li>Goulburn</li>
+        <li>Lismore</li>
+        <li>Armidale</li>
+        
+      </ul>
+    </div>
+  </div>
+</div>
+</div>
+
+
+
+    </> );
+}
+ 
+export default Residential;
