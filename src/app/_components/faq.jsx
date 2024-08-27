07be8ed8a60d8da183c30ea02e48289976e01813
zubaerSumon <zubaer@corexlab.dev>
2024-08-27 11:26:57 +0600

    faq updated

--- src/app/_components/faq.jsx
+++ src/app/_components/faq.jsx
@@ -1,4 +1,41 @@
 import * as React from "react";
+const faqs = [
+  {
+    question: "Consultation?",
+    answer:
+      "To schedule a consultation or service, contact us via phone, email, or using the website form. Our team will work with you to assess your needs and develop a comprehensive plan for your project.",
+  },
+  {
+    question: "Solutions?",
+    answer:
+      "We provide a range of solar solutions, including residential, commercial, and industrial systems. Our team of experts will design and install a customized solution that meets your energy needs and sustainability goals.",
+  },
+  {
+    question: "Maintenance?",
+    answer:
+      "Yes, we offer comprehensive maintenance and support for our installations. Our team provides regular system check-ups and preventive care to ensure your solar system operates efficiently and effectively throughout its lifespan.",
+  },
+  {
+    question: "Suitable?",
+    answer:
+      "Our team will assess your property by evaluating factors such as energy usage, roof space, and location. We'll provide a detailed analysis and personalized recommendations to decide if solar energy is a suitable and beneficial option for you.",
+  },
+  {
+    question: "Warranty?",
+    answer:
+      "Yes, our systems come with warranties. These include the quality of our installation and the performance of the solar panels. Contact us to find out more details about our warranty options.",
+  },
+  {
+    question: "System issues?",
+    answer:
+      "If you experience an issue with your system, contact us right away. Call or email our support team to report the problem, and we'll schedule an inspection or maintenance service to get your system running properly again.",
+  },
+  {
+    question: "Get a quote?",
+    answer:
+      "To get a quote, reach out to us through our website, phone, or email. Provide as many details as possible about your project, and we'll send you a detailed estimate based on your specific needs and requirements.",
+  },
+];
 
 function FAQ() {
   return (
@@ -10,38 +47,23 @@ function FAQ() {
       className="max-w-[1440px] ps-[256px] bg-fit bg-no-repeat  mt-[55px] bg-right w-full flex gap-10 justify-center items-center   text-slate-900 max-md:px-5"
     >
       <div className="flex flex-col flex-1 shrink self-stretch space-y-4 w-full basis-0    max-md:max-w-full">
-        <div className="text-3xl font-semibold text-[#0F172A] tracking-tight leading-tight max-md:max-w-full">
+        <h2 className="text-3xl font-semibold text-[#0F172A] tracking-tight leading-tight max-md:max-w-full">
           Frequently asked questions
-        </div>
-        <div className="flex pr-[256px] border flex-col items-start mt-4 w-full text-5xl font-extrabold tracking-tight leading-none max-md:max-w-full max-md:text-4xl">
-          <div class="group flex border">
-            <div class="cursor-pointer">Consultation?</div>
-            <div class="opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-              Detailed content about Consultation.
+        </h2>
+        <div className="flex pr-[256px] flex-col items-start w-full text-5xl font-extrabold tracking-tight leading-none max-md:max-w-full max-md:text-4xl">
+          {faqs.map((faq, i) => (
+            <div
+              key={i}
+              className="group w-full flex justify-between items-center"
+            >
+              <h1 className="cursor-pointer hover:text-[#1045AD] transition-text duration-500 text-[#94A3B8] text-[36px] font-semibold leading-12">
+                {faq.question}
+              </h1>
+              <p className="text-sm text-[#64748B] font-medium w-[551px] opacity-0 group-hover:opacity-100 transition-opacity duration-500">
+                {faq.answer}
+              </p>
             </div>
-          </div>
-          <div class="group flex border">
-            <div class="cursor-pointer">Solutions?</div>
-            <div class="opacity-0 group-hover:opacity-100 transition-opacity duration-1000">
-              Detailed content about Consultation.
-            </div>
-          </div>
-
-          <div className="space-y-2 text-[48px] text-[text[#0F172A] self-stretch mt-2 max-w-full whitespace-nowrap min-h-[52px] w-[370px] max-md:text-4xl">
-            Maintenance?
-          </div>
-          <div className="space-y-2 text-[48px] text-[text[#0F172A] self-stretch mt-2 max-w-full whitespace-nowrap min-h-[52px] w-[370px] max-md:text-4xl">
-            Suitable?
-          </div>
-          <div className="space-y-2 text-[48px] text-[text[#0F172A] self-stretch mt-2 max-w-full whitespace-nowrap min-h-[52px] w-[370px] max-md:text-4xl">
-            Warranty?
-          </div>
-          <div className="space-y-2 text-[48px] text-[text[#0F172A] self-stretch mt-2 max-w-full min-h-[52px] w-[370px] max-md:text-4xl">
-            System issues?
-          </div>
-          <div className="space-y-2 text-[48px] text-[text[#0F172A] self-stretch mt-2 max-w-full min-h-[52px] w-[370px] max-md:text-4xl">
-            Get a quote?
-          </div>
+          ))}
         </div>
       </div>
     </div>
