b7c4c2859696d543bdc9b4f7013d8fb75a670995
zubaerSumon <zubaer@corexlab.dev>
2024-08-27 16:35:26 +0600

    services modified

--- src/app/_components/services.jsx
+++ src/app/_components/services.jsx
@@ -1,7 +1,18 @@
 "use client";
+import { Factory } from "lucide-react";
+import { Calculator } from "lucide-react";
+import { BatteryCharging } from "lucide-react";
+import { Zap } from "lucide-react";
+import { Home } from "lucide-react";
 import { HomeIcon } from "lucide-react";
 import Image from "next/image";
-
+const cards = [
+  { title: "Residential", icon: Home },
+  { title: "Industrial", icon: Factory },
+  { title: "Commercial", icon: Calculator },
+  { title: "Solar panels", icon: Zap },
+  { title: "Solar batteries", icon: BatteryCharging },
+];
 export const Services = () => {
   return (
     <>
@@ -9,28 +20,32 @@ export const Services = () => {
         <h2 className="text-[30px] text-[#0F172A] mb-4 font-semibold leadding-[36px]">
           Our Services
         </h2>
-        <p className="text-sm text-[#0F172A] mb-8">
+        <p className="text-sm text-[#0F172A] mb-4">
           We offer a wide range of services that includes but not limited to
           wiring, rewiring, electrical inspection, solar panel installations and
           advanced battery systems.
         </p>
-        <div className="flex">
-          {Array(5)
-            .fill("")
-            .map((el, i) => (
-              <div ey={i} className="rounded-[8px] border border-[#E2E8F0]">
-                <div className="py-[42px] px-4 space-y-[10px]">
-                  <HomeIcon color="#EF8A22" size={24} />
-                  <p className="text-sm text-[#0F172A]">Residential</p>
-                </div>
+        <div className="flex  space-x-[-1px]">
+          {cards.map(({ title, icon: Icon }, i) => (
+            <div
+              key={i}
+              className="rounded-[8px] group overflow-hidden border-[#E2E8F0]"
+            >
+              <div className="py-[42px] px-4 space-y-[10px]">
+                <Icon color="#EF8A22" size={20} />
+                <p className="text-sm text-[#0F172A]">{title}</p>
+              </div>
+              <div className="h-[155px] overflow-hidden">
                 <Image
-                  src="/residential.png"
+                  src={`/ctg/image${i + 1}.png`}
                   height={155}
                   width={186}
-                  alt="Card Image"
+                  alt={`Image${i + 1}`}
+                  className="group-hover:scale-125 transition-transform duration-500"
                 />
               </div>
-            ))}
+            </div>
+          ))}
         </div>
       </div>
     </>
