0f3e1a5bd31f66a1634b86f572ab4cea9dd61086
sadat66 <imteaz.sadat@corexlab.dev>
2024-08-29 12:53:53 +0600

    updates residential

--- src/app/services/(service-layout)/residential/page.jsx
+++ src/app/services/(service-layout)/residential/page.jsx
@@ -1,43 +1,34 @@
-export default function HorizontalScroll() {
-  return (<>
-   <div className="flex justify-center bg-white mx-auto w-[928px] h-[2200px] pt-[128px]">
-   <div className="overflow-hidden w-auto">
-        <div className="flex flex-col max-w-[496px] h-[2264px] bg-white">
-        <h2 class="self-stretch text-black font-inter text-[30px] font-semibold leading-[36px] tracking-[-0.225px]">
-        Quick Air Condition Installation & Repair 
-</h2><p className="pt-[16px]">
+import Importance from "./importance";
+import Benefits from "./benefits";
+import WhyUs from "./why-us";
+  export default function HorizontalScroll() {
+    return (<>
+    <div className="flex justify-center bg-white mx-auto w-[928px] h-[2200px] pt-[128px]">
+    <div className="overflow-hidden w-auto">
+          <div className="flex flex-col max-w-[496px] h-[2264px] bg-white">
+          <h2 class="self-stretch text-black font-inter text-[30px] font-semibold leading-[36px] tracking-[-0.225px]">
+          Air Condition Installation & Repair 
+  </h2><p className="pt-[16px] text-[14px]">
 
-The bright summer sun outside the house makes the indoors quite uncomfortable. It affects regular life in the house and productivity at the office. The installation of air conditioning can help to resolve the problem. 
+  The bright summer sun outside the house makes the indoors quite uncomfortable. It affects regular life in the house and productivity at the office. The installation of air conditioning can help to resolve the problem. 
 
-</p>
+  </p>
+
+  <Importance/>
+  <Benefits/>
+  <WhyUs/>
 
 
-<h2 class="self-stretch text-black font-inter text-[30px] font-semibold leading-[36px] tracking-[-0.225px] pt-[72px]">
-Benefits of Having Air Conditions in NSW 
-</h2><p className="pt-[16px]">
-
-Air conditioning keeps a place cool and comfortable during NSW’s hot summers. It also improves air quality and boosts property value. 
-
-</p>
-
-<h3 class="self-stretch text-black font-inter text-[18px] font-semibold leading-[36px] tracking-[-0.225px] pt-[72px]">
-Increased Comfort During Extreme Weather
-</h3><p className="pt-[16px]">
-
-Air conditioners create a pleasant environment for employees, customers, and residents. This improves the quality of life at home and a satisfying working environment at the office. ACs ensure everyone stays comfortable no matter how hot it gets outside. 
-
-</p>
-
+          </div>
         </div>
-      </div>
 
-      {/* Right Div (Fixed with 48px gap) */}
-      <div className="sticky right-0 top-0 ml-[48px] w-[384px] h-[306px]">
-        <div className="h-full bg-green-200">
-          {/* Content of the right div */}
-          Fixed content here
+        {/* Right Div (Fixed with 48px gap) */}
+        <div className="sticky right-0 top-0 ml-[48px] w-[384px] h-[306px]">
+          <div className="h-full bg-green-200">
+            {/* Content of the right div */}
+            Fixed content here
+          </div>
         </div>
-      </div>
 
 
 
@@ -52,7 +43,7 @@ Air conditioners create a pleasant environment for employees, customers, and res
 
 
 
-   </div>
-    </>
-  );
-}
+    </div>
+      </>
+    );
+  }
---
+++ src/app/services/(service-layout)/residential/why-us.jsx
@@ -0,0 +1,41 @@
+const WhyUs = () => {
+    return ( <>
+
+        <h2 class="self-stretch text-black font-inter text-[30px] font-semibold leading-[36px] tracking-[-0.225px] pt-[72px]">
+        Why Choose Us?
+        </h2><p className="pt-[16px] text-[14px]">
+        
+        We stand out for our expertise and dedication to serve the community. Our team is highly trained, skilled, and committed to delivering top-quality results. We prioritize customer satisfaction and adhere to the highest safety standards.
+        
+        </p>
+        
+        <ul className="pt-[16px] text-[14px]" style={{ listStyleType: 'disc', paddingLeft: '20px' }}>
+              <li>
+                 Highly trained and certified technicians
+              </li>
+              <li>
+                 Committed to precision and quality
+              </li>
+              <li>
+                Focused on safety and reliability
+              </li>
+              <li>
+              Competitive and transparent pricing
+              </li>
+              <li>
+              Quick response times for urgent needs
+              </li>
+              <li>
+              Comprehensive warranty and follow-up support
+              </li>
+            
+            </ul>
+            <button className="h-[40px] w-[107px] bg-blue-950">
+
+            call us
+            </button>
+        
+            </> );
+}
+ 
+export default WhyUs;
