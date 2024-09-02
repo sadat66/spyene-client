007b0f225973c8fdafc59fedcac627c28208184c
sadat66 <imteaz.sadat@corexlab.dev>
2024-09-02 15:59:32 +0600

    update

--- src/components/navbar-items.jsx
+++ src/components/navbar-items.jsx
@@ -6,13 +6,15 @@ const NavbarItems = async () => {
     const services = await fetchServices();
   return (
     <div className="flex space-x-4 text-sm text-[#0F172A] font-medium cursor-pointer items-center min-h-[40px] hover:text-[#1045AD]">
-    <DropdownNavItem title="Services" menus={services} />
-    <Link className="hover:underline" href="/about">
-      About
-    </Link>
-    <Link className="hover:underline" href="/explore">
-      Explore
-    </Link>
+    
+    <DropdownNavItem title="Residential" menus={services} />
+    <DropdownNavItem title=" Industrial" menus={services} />
+    <DropdownNavItem title="Commercial" menus={services} />
+    <DropdownNavItem title="Solar panels" menus={services} />
+    <DropdownNavItem title="Solar batteries" menus={services} />
+    
+    
+   
   </div>
   )
 }
