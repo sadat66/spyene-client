5874a4badd607835a73dd97823eb6ee53809824b
sadat66 <imteaz.sadat@corexlab.dev>
2024-09-02 13:05:49 +0600

    responsive

--- src/app/_components/get-quotation.jsx
+++ src/app/_components/get-quotation.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import GetQuotationModal from "@/components/modals/get-quotation-modal";
 import { cn } from "@/lib/utils";
+
 const GetQuotation = ({ className }) => {
   return (
     <div
@@ -9,29 +10,29 @@ const GetQuotation = ({ className }) => {
         className
       )}
     >
-      <div className="flex w-full max-w-[928px] px-4 lg:px-0 gap-8 justify-center items-center self-stretch py-20 ">
-        <div className="flex flex-wrap flex-1 shrink gap-8 items-center self-stretch my-auto w-full basis-0 min-w-[240px] max-md:max-w-full">
-          <div className="flex flex-col flex-1 shrink justify-center self-stretch my-auto text-white basis-0 min-h-[97px] min-w-[240px] max-md:max-w-full">
-            <div className="flex gap-2 items-center self-start text-sm font-bold leading-none whitespace-nowrap">
-              <Image
-                src="/Explore.png" // Path to your image file in the public directory
-                alt="explorer" // A description of the image for accessibility
-                width={24} // Set the desired width of the image
-                height={24} // Set the desired height of the image
-              />
-              <div className="self-stretch my-auto">Explore</div>
-            </div>
-            <div className="mt-2 text-3xl font-semibold tracking-tight leading-tight">
-              Get a Free Quote
-            </div>
+      <div className="flex flex-col lg:flex-row w-full max-w-[928px] px-4 lg:px-0 gap-8 justify-center items-center py-20">
+        <div className="flex flex-col flex-1 gap-8 items-start lg:items-center text-white">
+          <div className="flex gap-2 items-center text-sm font-bold leading-none">
+            <Image
+              src="/Explore.png" // Path to your image file in the public directory
+              alt="explorer" // A description of the image for accessibility
+              width={24} // Set the desired width of the image
+              height={24} // Set the desired height of the image
+            />
+            <div>Explore</div>
           </div>
-          <div className="flex flex-col flex-1 shrink justify-center self-stretch my-auto text-sm basis-0 min-w-[240px] max-md:max-w-full">
-            <div className="leading-6 text-slate-50 max-md:max-w-full pb-[20px]">
-              Spyene Technology has dedicated, experienced, and trained staff.
-              We ensure the best services across the state.
-            </div>
-            <GetQuotationModal />
+          <div className="text-3xl font-semibold tracking-tight leading-tight">
+            Get a Free Quote
           </div>
+        </div>
+        <div className="flex flex-col flex-1 text-sm">
+          <div className="leading-6 text-slate-50 mb-4">
+            Spyene Technology has dedicated, experienced, and trained staff.
+            We ensure the best services across the state.
+          </div>
+          
+            <GetQuotationModal className="w-full lg:w-auto" />
+          
         </div>
       </div>
     </div>
